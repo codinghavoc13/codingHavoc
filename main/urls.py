16db9e54e96d276c3da3eaeb799b24b1b1d224f8
sean <seanhayes13@hotmail.com>
2023-11-15 19:38:54 -0700

    202311151938

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from main import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("links/", views.links, name="links"),
+    path("contact/", views.contact, name="contact"),
 ]
