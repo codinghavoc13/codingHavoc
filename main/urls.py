58d0b2566725cc87b9707d0a71f2c0ab06ea6454
sean <seanhayes13@hotmail.com>
2023-11-17 15:26:00 -0700

    202311171525

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,5 @@ from main import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("links/", views.links, name="links"),
-    path("contact/", views.contact, name="contact"),
-    path("hobbies/", views.hobbies, name="hobbies"),
     path("projects/", views.projects, name="projects"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,39 +1,22 @@
 from django.shortcuts import render
 import json
-
-linkList = [
-    {
-        "site": "https://www.linkedin.com/in/sean-hayes-1a609888/",
-        "icon": "/main/image/linkedin.511x512.png",
-        "altText":"LinkedIn Profile",
-        "randomText": "A"
-    },{
-        "site": "https://github.com/seanhayes13/dev",
-        "icon": "main/image/github-6980894_640.png",
-        "altText":"GitHub Dev Repo",
-        "randomText": "B"
-    },{
-        "site": "https://www.hackerrank.com/profile/seanhayes13",
-        "icon": "main/image/600px-HackerRank_Icon-1000px.png",
-        "altText":"HackerRank Profile",
-        "randomText": "C"
-    }
-]
+import numpy
 
 def home(request):
-    return render(request,"main/index.html")
+    data = open('main/static/main/data.json')
+    data1 = data.read()
+    data2 = json.loads(data1)
+    
+    hobbies = randomiseHobbies(data2['hobbyInterests'])
+    return render(request,"main/index.html",{'linkResults':data2['linkList'],'hobbyList0':hobbies[0],'hobbyList1':hobbies[1],'hobbyList2':hobbies[2]})
 
 def about(request):
     return render(request,"main/about.html")
 
-def links(request):
-    return render(request,"main/links.html",{'linkResults':linkList})
-
-def contact(request):
-    return render(request,"main/contact.html")
-
-def hobbies(request):
-    return render(request,"main/hobbies.html")
-
 def projects(request):
     return render(request, "main/projects.html")
+
+def randomiseHobbies(arr):
+    numpy.random.shuffle(arr)
+    result = numpy.array_split(arr,3)
+    return result
