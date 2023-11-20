f251b665232df2367eae51eb56ee9294551e1b0a
sean <seanhayes13@hotmail.com>
2023-11-20 12:26:04 -0700

    202311201226

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("/", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("projects/", views.projects, name="projects"),
 ]
