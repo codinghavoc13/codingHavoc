2dff9bed4cba046ba1abab2888528d656b2de3ca
sean <seanhayes13@hotmail.com>
2023-11-19 21:01:04 -0700

    202311192100 - testing os check

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 import json
 import numpy
+import os
 
 def home(request):
+    if "CI" in os.environ:
+        print("HEROKU")
+    else:
+        print("LOCAL")
     data = open('main/static/main/data.json')
     data1 = data.read()
     data2 = json.loads(data1)
