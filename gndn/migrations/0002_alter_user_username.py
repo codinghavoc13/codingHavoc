8122d8da0dfb75f74fc6f2483f691961c419c72d
Sean Hayes <seanhayes13@hotmail.com>
2023-12-26 14:52:12 -0700

    202312261452

--- gndn/migrations/0002_alter_user_username.py
+++ gndn/migrations/0002_alter_user_username.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gndn', '0005_delete_loginstatus'),
+        ('gndn', '0001_initial'),
     ]
 
     operations = [
