6321c110ade6091944989a642305a280900ffb16
Sean Hayes <seanhayes13@hotmail.com>
2024-04-15 06:43:25 -0600

    202404150628 disabling some elements while I fight with heroku

--- codingHavoc/settings.py
+++ codingHavoc/settings.py
@@ -108,16 +108,16 @@ WSGI_APPLICATION = 'codingHavoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('DB_DB'),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.getenv('DB_DB'),
     #     'USER': os.getenv('DB_USER'),
     #     'PASSWORD': os.getenv('DB_PW'),
     #     'HOST':os.getenv('DB_HOST'),
     #     'PORT':'5432',
     #     # comment out the line below when testing locally
     #     # 'OPTIONS': {'sslmode': 'require'},
-    # }
+    }
 }
 
 
